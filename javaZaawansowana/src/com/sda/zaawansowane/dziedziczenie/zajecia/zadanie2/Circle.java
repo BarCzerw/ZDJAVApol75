2bf451caae5bfd31a5e228aeb9104d9cb36e1eaa
Bartłomiej <czerwonkabartlomiej@gmail.com>
2021-02-09 20:56:41 +0100

    Dziedziczenie - zajecia - zadanie 2

---
+++ javaZaawansowana/src/com/sda/zaawansowane/dziedziczenie/zajecia/zadanie2/Circle.java
@@ -0,0 +1,29 @@
+package com.sda.zaawansowane.dziedziczenie.zajecia.zadanie2;
+
+public class Circle {
+
+    final protected static double PI=3.1415;
+    protected double radius;
+
+    /*final public Circle(double radius) {
+        this.radius = radius; //ERROR not allowed here
+    }*/
+
+    public Circle(final double radius) {
+        //radius*=2; ERROR because argument is final
+        this.radius = radius;
+    }
+
+    final public double calculateArea(){
+        return PI*radius*radius;
+    }
+
+    final public double calculateArea(boolean isPrecise){
+        if (isPrecise){
+            return calculateArea();
+        }else {
+            return 3*radius*radius;
+        }
+    }
+
+}
