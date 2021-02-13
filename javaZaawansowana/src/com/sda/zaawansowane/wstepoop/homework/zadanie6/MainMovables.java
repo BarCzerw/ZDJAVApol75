9f09dd06b694f695121790d1928155f53857062b
Bartłomiej <czerwonkabartlomiej@gmail.com>
2021-02-13 09:23:18 +0100

    HomeWork Zadanie 6

---
+++ javaZaawansowana/src/com/sda/zaawansowane/wstepoop/homework/zadanie6/MainMovables.java
@@ -0,0 +1,46 @@
+package com.sda.zaawansowane.wstepoop.homework.zadanie6;
+
+public class MainMovables {
+
+    public static void main(String[] args) {
+
+        Movable[] movables = new Movable[]{
+                new MovablePoint(),
+                new MovableCircle(),
+                new MovablePoint(-4,8,2,2),
+                new MovableCircle(5,new MovablePoint(-3,6,3,3))
+        };
+
+        System.out.println("Initial position");
+
+        for (Movable movable : movables) {
+            System.out.println(movable);
+        }
+
+        for (Movable movable : movables) {
+            movable.moveDown();
+            movable.moveRight();
+        }
+
+        System.out.println("-----------------------\nAfter moving down and right");
+        for (Movable movable : movables) {
+            System.out.println(movable);
+        }
+
+        System.out.println("-----------------------\nBut if you pass instance of existing MovablePoint this happens");
+
+        MovablePoint p1 = new MovablePoint(5,5,99,99);
+        MovableCircle c1 = new MovableCircle(5,p1);
+
+        System.out.println("Inital point and circle");
+        System.out.println(p1+"\n"+c1);
+        System.out.println("Moving POINT ONLY up and left");
+        p1.moveUp();
+        p1.moveLeft();
+        System.out.println(p1+"\n"+c1);
+
+        System.out.println("-----------------------\nSolution is to create new MovablePoint in MovableCircle's constructor using coordinates and speed of passed argument");
+
+    }
+
+}
---
+++ javaZaawansowana/src/com/sda/zaawansowane/wstepoop/homework/zadanie6/Movable.java
@@ -0,0 +1,10 @@
+package com.sda.zaawansowane.wstepoop.homework.zadanie6;
+
+public interface Movable {
+
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+
+}
