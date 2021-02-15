f8b2bc2452a5c09a7202ffb352d8d6787e7b00d3
Bartłomiej <czerwonkabartlomiej@gmail.com>
2021-02-15 20:36:38 +0100

    Zadania z prezentacji - interfejsy zad 1 - typy generyczne - garaz

---
+++ javaZaawansowana/src/com/sda/zaawansowane/wstepoop/zajecia/zadanie10/BMW.java
@@ -0,0 +1,7 @@
+package com.sda.zaawansowane.wstepoop.zajecia.zadanie10;
+
+public class BMW extends Car{
+    public BMW(String model, String color) {
+        super("BMW", model, color);
+    }
+}
---
+++ javaZaawansowana/src/com/sda/zaawansowane/wstepoop/zajecia/zadanie10/Car.java
@@ -0,0 +1,28 @@
+package com.sda.zaawansowane.wstepoop.zajecia.zadanie10;
+
+public class Car {
+
+    protected String manufacturer;
+    protected String model;
+    protected String color;
+
+    public Car(String manufacturer, String model, String color) {
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.color = color;
+    }
+
+    public void changeColor(String color) {
+        if (!color.equals(this.color)) {
+            this.color = color;
+            System.out.println("Car repainted to " + color);
+        } else {
+            System.out.println("Car already painted in " + color);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return color + " " + manufacturer + " " + model;
+    }
+}
