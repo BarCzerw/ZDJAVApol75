7c2427c26ae8e7496690aad3b8c3a6157ea7d3d5
Bartłomiej <czerwonkabartlomiej@gmail.com>
2021-02-15 21:06:53 +0100

    Zadania z prezentacji - interfejsy zad 2 - typy generyczne - MyList.java

---
+++ javaZaawansowana/src/com/sda/zaawansowane/wstepoop/zajecia/zadanie11/MyList.java
@@ -0,0 +1,50 @@
+package com.sda.zaawansowane.wstepoop.zajecia.zadanie11;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class MyList<E> {
+
+    private int itemsCount;
+    private E[] items;
+
+    public MyList(int maxItems) {
+        items = (E[])new Object[maxItems];
+        itemsCount = 0;
+    }
+
+    public boolean contains(E element) {
+        for (int i = 0; i < itemsCount; i++) {
+            if (element.equals(items[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public int size() {
+        return itemsCount;
+    }
+
+    public boolean addItem(E element) {
+        if (itemsCount < items.length) {
+            items[itemsCount++] = element;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder text = new StringBuilder("[");
+        for (int i = 0; i < itemsCount; i++) {
+            text.append(items[i]);
+            if (i!=itemsCount-1){
+                text.append(", ");
+            }
+        }
+        text.append("]");
+        return text.toString();
+    }
+}
