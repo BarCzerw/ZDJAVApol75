9af447a1e9ecfa740fe344d9483c1b2efae78b37
BarCzerw <czerwonkabartlomiej@gmail.com>
2021-02-22 15:50:44 +0100

    Homework - generyki - zadanie 1 - Pair.java

---
+++ javaZaawansowana/src/com/sda/zaawansowane/wstepoop/homework/generics/zadanie1/PairMain.java
@@ -0,0 +1,21 @@
+package com.sda.zaawansowane.wstepoop.homework.generics.zadanie1;
+
+public class PairMain {
+
+    public static void main(String[] args) {
+
+        Pair<Integer> integerPair = new Pair<>(-5,9);
+
+        System.out.println(integerPair);
+
+        System.out.println(integerPair.getFirst());
+        System.out.println(integerPair.getSecond());
+
+        integerPair.setFirst(6);
+        integerPair.setSecond(-48);
+
+        System.out.println(integerPair);
+
+    }
+
+}
